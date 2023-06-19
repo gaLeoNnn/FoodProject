760af7f6a667504d228b342fa0c914dcd9c53357
Ali <Zhanabekov.ali@gmail.com>
2023-06-19 11:25:24 +0600

    added script tabs

---
+++ js/script.js
@@ -0,0 +1,35 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const tabsParent = document.querySelector(".tabheader__items"),
+    tabs = document.querySelectorAll(".tabheader__item"),
+    tabContent = document.querySelectorAll(".tabcontent");
+
+  function hideTabContent() {
+    tabContent.forEach((item) => {
+      item.classList.add("hide");
+      item.classList.remove("show", "fade");
+    });
+    tabs.forEach((item) => item.classList.remove("tabheader__item_active"));
+  }
+
+  function showTabContent(i = 0) {
+    console.log(i);
+    tabContent[i].classList.remove("hide");
+    tabContent[i].classList.add("show", "fade");
+    tabs[i].classList.add("tabheader__item_active");
+  }
+
+  hideTabContent();
+  showTabContent();
+
+  tabsParent.addEventListener("click", (e) => {
+    console.log(e);
+    if (e.target && e.target.classList.contains("tabheader__item")) {
+      tabs.forEach((item, index) => {
+        if (item === e.target) {
+          hideTabContent();
+          showTabContent(index);
+        }
+      });
+    }
+  });
+});
