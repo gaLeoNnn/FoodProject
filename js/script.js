34261ed61d0176d4989b8182a0eb41548edc8f29
Ali <Zhanabekov.ali@gmail.com>
2023-07-01 18:41:10 +0600

    added slider

--- js/script.js
+++ js/script.js
@@ -35,7 +35,7 @@ window.addEventListener("DOMContentLoaded", () => {
 
   //Timer
 
-  const endTime = "2023-07-01";
+  const endTime = "2025-07-01";
 
   function getTimeRemaining(endtime) {
     const t = Date.parse(endtime) - Date.parse(new Date()),
@@ -168,35 +168,23 @@ window.addEventListener("DOMContentLoaded", () => {
     }
   }
 
-  const menu = new MenuCard(
-    "img/tabs/vegy.jpg",
-    "vegy",
-    'Меню "Фитнес"',
-    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это  абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-    9,
-    ".menu .container"
+  const getResourse = async (url) => {
+    const res = await fetch(url);
+    return await res.json();
+  };
+
+  getResourse("http://localhost:3000/menu").then((data) =>
+    data.forEach(({ img, altimg, title, descr, price }) => {
+      new MenuCard(
+        img,
+        altimg,
+        title,
+        descr,
+        price,
+        ".menu .container"
+      ).render();
+    })
   );
-  menu.render();
-
-  new MenuCard(
-    "img/tabs/elite.jpg",
-    "elite",
-    "Меню 'Премиум'",
-    "В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты,фрукты - ресторанное меню без похода в ресторан!",
-    10,
-    ".menu .container",
-    "menu__item"
-  ).render();
-
-  new MenuCard(
-    "img/tabs/post.jpg",
-    "post",
-    'Меню "Постное"',
-    "   Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля,овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.",
-    11,
-    ".menu .container",
-    "menu__item"
-  ).render();
 
   ///Post request
   const forms = document.querySelectorAll("form");
@@ -207,10 +195,21 @@ window.addEventListener("DOMContentLoaded", () => {
   };
 
   forms.forEach((form) => {
-    postReq(form);
+    bindPostReq(form);
   });
 
-  function postReq(form) {
+  const postReq = async (url, obj) => {
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: obj,
+    });
+    return await res.json();
+  };
+
+  function bindPostReq(form) {
     form.addEventListener("submit", (e) => {
       e.preventDefault();
 
@@ -222,21 +221,10 @@ window.addEventListener("DOMContentLoaded", () => {
       form.insertAdjacentElement("afterend", statusMessage); /// вставить после form блока
 
       const formData = new FormData(form);
-      const obj = {};
 
-      formData.forEach((item, index) => {
-        obj[index] = item;
-      });
-      console.log(obj);
+      const json = JSON.stringify(Object.fromEntries(formData.entries()));
 
-      fetch("server.php", {
-        method: "POST",
-        headers: {
-          "Content-type": "application/json",
-        },
-        body: JSON.stringify(obj),
-      })
-        .then((data) => data.text())
+      postReq("http://localhost:3000/requests", json)
         .then((data) => {
           console.log(data);
           showThanksModal(message.success);
@@ -255,6 +243,7 @@ window.addEventListener("DOMContentLoaded", () => {
     const modalDialog = document.querySelector(".modal__dialog");
     modalDialog.classList.add("hide");
     openModal();
+
     const thanksModal = document.createElement("div");
     thanksModal.classList.add("modal__dialog");
     thanksModal.innerHTML = `<div class="modal__content"> 
@@ -270,4 +259,144 @@ window.addEventListener("DOMContentLoaded", () => {
       closeModal();
     }, 4000);
   }
+
+  /// SLIDER
+
+  const wrapper = document.querySelector(".offer__slider-wrapper"),
+    slides = document.querySelectorAll(".offer__slide"),
+    inner = document.querySelector(".offer__slider-inner"),
+    btnPrev = document.querySelector(".offer__slider-prev"),
+    btnNext = document.querySelector(".offer__slider-next"),
+    total = document.querySelector("#total"),
+    current = document.querySelector("#current"),
+    parentWidth = wrapper.offsetWidth;
+  //dots
+  const parentAbsolute = document.querySelector(".offer__slider"),
+    wrapperDots = document.createElement("ol"),
+    dots = [];
+
+  inner.style.width = `${parentWidth * slides.length}px`;
+  inner.style.display = "flex";
+  inner.style.transition = "0.5s all";
+
+  slides.forEach((slide) => {
+    slide.style.width = parentWidth + "px";
+  });
+
+  wrapper.style.overflow = "hidden";
+
+  let index = 1;
+  let offset = 0;
+
+  if (slides.length < 10) {
+    total.textContent = `0${slides.length}`;
+    current.textContent = `0${index}`;
+  } else {
+    total.textContent = slides.length;
+    current.textContent = index;
+  }
+
+  function setCurrentSlide() {
+    if (index < 10) {
+      current.textContent = `0${index}`;
+    } else {
+      current.textContent = index;
+    }
+  }
+
+  function setDots(n) {
+    dots.forEach((dot) => {
+      dot.style.opacity = 0.5;
+      dots[n - 1].style.opacity = 1;
+    });
+  }
+
+  btnNext.addEventListener("click", () => {
+    if (offset === parentWidth * (slides.length - 1)) {
+      offset = 0;
+    } else {
+      offset += parentWidth;
+    }
+    inner.style.transform = `translateX(${-offset}px)`;
+
+    if (slides.length === index) {
+      index = 1;
+    } else {
+      index++;
+    }
+
+    setCurrentSlide();
+    setDots(index);
+  });
+
+  btnPrev.addEventListener("click", () => {
+    if (offset == 0) {
+      offset = parentWidth * (slides.length - 1);
+    } else {
+      offset -= parentWidth;
+    }
+    inner.style.transform = `translateX(${-offset}px)`;
+
+    if (index === 1) {
+      index = slides.length;
+    } else {
+      index--;
+    }
+
+    setCurrentSlide();
+    setDots();
+  });
+
+  parentAbsolute.style.position = "relative";
+
+  wrapperDots.style.cssText = `
+   position: absolute;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    z-index: 15;
+    display: flex;
+    justify-content: center;
+    margin-right: 15%;
+    margin-left: 15%;
+    list-style: none;
+  `;
+  parentAbsolute.append(wrapperDots);
+
+  slides.forEach((slide, index) => {
+    const dot = document.createElement("li");
+    dot.classList.add("dot");
+    dot.setAttribute("data-slide-to", index + 1);
+    dot.style.cssText = `
+    box-sizing: content-box;
+    flex: 0 1 auto;
+    width: 30px;
+    height: 6px;
+    margin-right: 3px;
+    margin-left: 3px;
+    cursor: pointer;
+    background-color: #fff;
+    background-clip: padding-box;
+    border-top: 10px solid transparent;
+    border-bottom: 10px solid transparent;
+    opacity: .5;
+    transition: opacity .6s ease;
+    `;
+
+    wrapperDots.append(dot);
+    dots.push(dot);
+  });
+
+  setDots(index);
+
+  dots.forEach((dot) => {
+    dot.addEventListener("click", (e) => {
+      const number = e.target.getAttribute("data-slide-to");
+      index = number;
+      setCurrentSlide();
+      setDots(number);
+      offset = parentWidth * (number - 1);
+      inner.style.transform = `translateX(${-offset}px)`;
+    });
+  });
 });
